b4bfc71cac76aaf648c146971ca6a7c3391d288d
2EXP <26007713+2EXP@users.noreply.github.com>
2023-05-27 02:58:41 +0800

    l-smash 2.18.0 (new formula)

--- Formula/l-smash.rb
+++ Formula/l-smash.rb
@@ -1,9 +1,9 @@
 class LSmash < Formula
-  desc ""
-  homepage ""
+  desc "Vimeo L-SMASH fork"
+  homepage "https://github.com/vimeo/l-smash"
   url "https://github.com/vimeo/l-smash/archive/v2.18.0.tar.gz"
   sha256 "9a2ae612e3d84116225725013cda2a6e996f65767d923fc07bb67025de9d2215"
-  license ""
+  license "ISC"
 
 
   def install
